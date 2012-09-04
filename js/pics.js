012f997afaf75313f0c99c85ffdb0808f2f41c0f
Michael Francis <edude03@gmail.com>
2012-09-04 13:41:26 -0400

    Initial Commit

---
+++ js/pics.js
@@ -0,0 +1,46 @@
+var myApp = angular.module('myApp', []);
+
+myApp.filter('fromNow', function() {
+	return function(input) {
+		return moment.unix(input).fromNow();
+	}
+});
+
+myApp.filter('imgur', function(){
+	return function(input) {
+		//imgur.com + 5 letter image code
+		if (input.length == 22) {
+			return input += '.jpg';
+		}
+		return input;
+	}
+});
+
+myApp.filter('comments', function() {
+	return function(input) {
+		return "http://reddit.com" + input;
+	}
+});
+
+function PicsCtrl($scope, $http) {
+	$scope.reload = function() {
+		var subreddit = $('#subreddit').val().length > 0 ? $('#subreddit').val() : 'pics';
+		var url = 'http://api.reddit.com/r/' + subreddit + '.json?jsonp=JSON_CALLBACK';
+		alert(url);
+		$scope.loadContent(url);
+	};
+
+	$scope.nextPage = function() {
+		var url = 'http://api.reddit.com/r/pics.json?jsonp=JSON_CALLBACK&count=25&after=' . $scope.after;
+		alert(url);
+	};
+
+	$scope.loadContent = function(url) {
+		$http.jsonp(url).success(function(data){
+			$scope.pics = data.data.children;
+			$scope.after = data.after;
+		});	
+	};		
+
+	$scope.loadContent('http://api.reddit.com/r/pics.json?jsonp=JSON_CALLBACK');	
+}
